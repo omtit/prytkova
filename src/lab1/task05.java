c68555a7bc51382e94e6ee2e7f070413679d67e7
zenfoliooksana <zenfolio.oksana@gmail.com>
2017-11-27 19:52:58 +0600

    task05

---
+++ src/lab1/task05.java
@@ -0,0 +1,13 @@
+package lab1;
+import java.util.Scanner;
+public class task05 {
+    public static void main(String[] args) {
+
+        Scanner s = new Scanner(System.in);
+        String a;
+        a = s.nextLine();
+        String b;
+        b = s.nextLine();
+        System.out.println(b +" " + a);
+    }
+}
