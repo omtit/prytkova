50a11d38f7cc16ebfbd005e4174d590f6c3282d8
zenfoliooksana <zenfolio.oksana@gmail.com>
2017-11-27 20:19:58 +0600

    добавлен 5 6 9

---
+++ src/lab1/task06.java
@@ -0,0 +1,9 @@
+package lab1;
+
+public class task06 {
+    public static void main(String[] args) {
+        System.out.println(Math.PI);
+        System.out.printf("Значение пи:%.4f\n",Math.PI);
+        System.out.printf("Значение пи:%.4f\n Значение е:%.4f",Math.PI, Math.E);
+    }
+}
--- src/lab1/task09.java
+++ src/lab1/task09.java
@@ -1,14 +1,12 @@
 package lab1;
 import java.util.Scanner;
-public class task05 {
+public class task09 {
     public static void main(String[] args) {
 
         Scanner s = new Scanner(System.in);
         String a;
         a = s.nextLine();
-        String b;
-        b = s.nextLine();
-        System.out.println(b +" " + a);
-        System.out.printf("%s %s",b,a);
+        System.out.println("SELECT first_name, last_name, group\n" +
+                "FROM students WHERE student_id = '" + a + "';");
     }
-}
+    }
