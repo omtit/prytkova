20538cc7d6e0b6d1f9d77357214b1bd5eb5a2c49
zenfoliooksana <zenfolio.oksana@gmail.com>
2017-12-21 00:02:59 +0600

    9

---
+++ src/lab4/task09.java
@@ -0,0 +1,14 @@
+package lab4;
+
+public class task09 {
+    public static void main(String[] args) {
+        int a=1;
+        int r=7;
+        while (a<10) {
+            System.out.println(a +" x 7 = "+r);
+            a=a+1;
+            r=a*7;
+
+        }
+    }
+}
