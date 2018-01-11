ee80e3a639a8032cf016e0adb92e5bb0ba55bb4e
zenfoliooksana <zenfolio.oksana@gmail.com>
2018-01-11 17:57:41 +0600

    lab5

--- src/lab5/task13.java
+++ src/lab5/task13.java
@@ -6,9 +6,24 @@ public class task13 {
     public static void main(String[] args) {
         Scanner s = new Scanner(System.in);
         String data = s.nextLine();
-        String [] arr = data.split(" ");
-        for (int i = 0; i<arr.length; i++)        {
-        String t = arr[i]; 
+        String[] arr = data.split(" ");
+        int i = 0;
+        int count = 0;
+        while (i < arr.length) {
+            String n = arr[i];
+            for (int z = 0; z < arr.length; z++) {
+                //String t = arr[z];
+
+                if (n.equals(arr)) {
+                    count++;
+                }
+            }
+            i++;
+        }
+        System.out.println("Повторений: " + count);
+
 
     }
-}}
+    //    System.out.println(" повторений нет");
+}
+
