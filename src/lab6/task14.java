db6ad89e1d767885cd7e78d469548383cebcc612
zenfoliooksana <zenfolio.oksana@gmail.com>
2018-01-19 12:21:46 +0600

    cnhjrb

--- src/lab6/task14.java
+++ src/lab6/task14.java
@@ -1,4 +1,15 @@
 package lab6;
 
+import java.util.Scanner;
+
 public class task14 {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        String str = s.nextLine();
+        String [] str1 = str.split(" ");
+        String str2 = str.replaceFirst("\"", "[");
+        String str3 = str2.replace("\"","]");
+        System.out.println(str3);
+
+    }
 }
---
+++ src/lab6/task15.java
@@ -0,0 +1,10 @@
+package lab6;
+
+import java.util.Scanner;
+
+public class task15 {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        String str = s.nextLine();
+    }
+}
